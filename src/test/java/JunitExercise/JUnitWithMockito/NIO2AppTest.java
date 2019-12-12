26d4efc77a3b664bd0b936a8a5ccc8349fd973c9
Amitkumar35b14f <amit.kumar35b14f@cognizant.com>
2019-12-12 23:11:21 +0530

    Update NIO2AppTest.java

--- src/test/java/JunitExercise/JUnitWithMockito/NIO2AppTest.java
+++ src/test/java/JunitExercise/JUnitWithMockito/NIO2AppTest.java
@@ -1,44 +1,30 @@
 package JunitExercise.JUnitWithMockito;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import JunitExercise.JUnitWithMockito.nios2.NIO2Exercise;
 @RunWith(MockitoJUnitRunner.class)
 public class NIO2AppTest {
-	@InjectMocks
+	@Mock
 	NIO2Exercise Nio2Obj;
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		System.out.println("Before Execution of Class Or All Methods");
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		System.out.println("After Execution of class/ All Methods");
-	}
 
 	@Before
 	public void setUp() throws Exception {
-		System.out.println("Before All Methos");
+		System.out.println("@Before ->Before All Methos");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		System.out.println("After All Methods");
+		System.out.println("@After ->After All Methods");
 	}
 
 	@Test
@@ -46,19 +32,7 @@ public class NIO2AppTest {
 		Map<String,LocalDate> expected=new HashMap<>();
 		expected.put("D-cold Total", LocalDate.of(2001, 02, 10));
 		expected.put("D-cold Total1", LocalDate.of(2000, 07, 10));
-		
-		assertEquals(expected, Nio2Obj.getExpiredTablets("tablet_details.txt", "def"));
+		assertEquals("Test Case For getExpiredTablets",expected, Nio2Obj.getExpiredTablets("tablet_details.txt", "def"));
 	}
-	@Test
-	public void testsearchFile() {
-		//when(Nio2Obj.searchFile("hidden.txt","src//file//data//")).thenReturn(true);
-		//when(obj.searchFile("hidden.txt","src//file//data//")).thenReturn(true);
-	}
-	@Test
-	public void testsearchFile1() {
-		//when(Nio2Obj.searchFile("hidden.txt","src//file//data//")).thenReturn(true);
-		//when(obj.searchFile("hidden.txt","src//file//data//")).thenReturn(true);
-	}
-
 
 }
